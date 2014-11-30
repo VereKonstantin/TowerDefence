e26876236bff7a443468f5918db14825f407fae6
koveres <andrei1@andrei-romashchenkos-macbook.local>
2014-11-30 13:22:56 +0300

    Initial commit from local git

---
+++ CLTese/CLTese/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  CLTese
+//
+//  Created by koveres on 30.11.14.
+//  Copyright (c) 2014 Kostya. All rights reserved.
+//
+
+import Foundation
+
+println("Hello, World!")
+
---
+++ TD/TD.h
@@ -0,0 +1,19 @@
+//
+//  TD.h
+//  TD
+//
+//  Created by koveres on 30.11.14.
+//  Copyright (c) 2014 Kostya. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for TD.
+FOUNDATION_EXPORT double TDVersionNumber;
+
+//! Project version string for TD.
+FOUNDATION_EXPORT const unsigned char TDVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TD/PublicHeader.h>
+
+
